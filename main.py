276db6153c090a5a945680572b13ed3fadeefc57
KimByeongJin :: HoldimProvae <relasers@gmail.com>
2017-05-19 21:58:35 +0900

    UI

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+from tkinter import *
 import xml
+
+
+#########################################################################################################################
+root = Tk()
+root.geometry('640x480+200+200')
+theLabel = Label(root, text= "Alternative Military Service")
+theLabel.pack()
+
+topFrame = Frame(root)
+topFrame.pack()
+buttonFrame = Frame(root)
+buttonFrame.pack(side=LEFT)
+
+SearchButton = Button(topFrame, text = "검색", fg="red")
+SortButton = Button(topFrame, text = "정렬", fg="red")
+
+
+
+
+#SearchButton.pack()
+#SortButton.pack()
+SearchButton.grid(row=1, column=1, columnspan=2)
+SortButton.grid(row=2, column=1, columnspan=2)
+#SearchButton.place(x=0, y=0)
+#SortButton.place(x=0, y=100)
+
+
+
+
+
+
+#####################################################################################################################
+root.mainloop()
